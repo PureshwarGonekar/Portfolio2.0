ee76c21d8e665ff9b2bdcdf831d5c9ca226f4761
Pureshwar Gonekar <pureshwargonekar7@gmail.com>
2024-02-25 20:28:43 +0530

    initial commit

---
+++ src/components/About/About.tsx
@@ -0,0 +1,95 @@
+import { Container } from "./styles";
+import PureshwarGonekar from "../../assets/photo.png";
+import wordpress from "../../assets/wordpress.svg";
+import shopify from "../../assets/shopify.svg";
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+import vueIcon from "../../assets/vue-icon.svg";
+import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import tailwindIcon from "../../assets/tailwind.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft" >
+          <h2>About me</h2>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
+          <p>
+          Hi there! I'm Pureshwar Gonekar, a skilled MERN stack developer with expertise in React.js, Node.js, MongoDB and Express.js.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <p>
+          I specialize in crafting dynamic web applications with seamless user experiences.</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <p>
+          Let's collaborate to bring your projects to life!</p>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+          <h3>Here are my main skills:</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
+              <img src={typescriptIcon} alt="Typescript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={boostrapIcon} alt="bootstrap" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={tailwindIcon} alt="taiwind" />
+            </ScrollAnimation>
+          </div>
+        </div>
+      </div>
+      <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
+          <img src={PureshwarGonekar} alt="Pureshwar Gonekar" />
+        </ScrollAnimation>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Project/Project.tsx
@@ -0,0 +1,85 @@
+import { Container } from "./styles";
+// import githubIcon from "../../assets/github.svg"
+// import DownloadApp from '../../assets/download.png'
+import externalLink from "../../assets/external-link.svg"
+import ScrollAnimation from "react-animate-on-scroll";
+
+import { ThreeDCardDemo } from "./ProjectCard";
+
+import pglife from '../../assets/project/pglife.png'
+import pchess from '../../assets/project/pchess.png'
+import cellfusion from '../../assets/project/cellfusion.png'
+import SLD from '../../assets/project/SLD.png'
+import protfolio1 from '../../assets/project/protfolio1.png'
+import protfolio2 from '../../assets/project/protfolio2.png'
+import userManager from '../../assets/project/userManager.png'
+export function Project() {
+  return (
+
+    <Container id="project">
+      <h2 className=" text-red-400">My Projects</h2>
+      <div className="projects">
+
+
+        <ThreeDCardDemo 
+          title={'Pchess - A mulitplayer online Chess game'} 
+          tech={'React.js, Node.js, ExpressJS, Socket.IO, CSS'}
+          image={pchess}
+
+          link={'https://pchess-pureshwar.vercel.app/'}
+        />
+
+        <ThreeDCardDemo 
+          title={'User Manager - Admin-Managed User Access'} 
+          tech={'HTML, CSS, JavaScript, EJS, MongoDB'}
+          image={userManager}
+          link={'https://github.com/PureshwarGonekar/User-Management-System'}
+        />
+
+        <ThreeDCardDemo 
+          title={'PG Life - A PG listings website across different regions'} 
+          tech={'HTML, CSS, Bootstrap, JavaScript, PHP, MySQL'}
+          image={pglife}
+          link={'http://pglife-pureshwar.42web.io/'}
+        />
+
+
+        <ThreeDCardDemo 
+          title={'CellFusion - A mobile, gadget selling website'} 
+          tech={'HTML, CSS, Bootstrap, JavaScript, PHP, MySQL'}
+          image={cellfusion}
+          link={'https://cellfusion.000webhostapp.com/'}
+        />
+
+        <ThreeDCardDemo 
+          title={'Sign Language Detection System'} 
+          tech={'Python, Keras, Pandas, Numpy'}
+          image={SLD}
+          link={'https://github.com/PureshwarGonekar/Sign-Language-Detection'}
+        />
+
+        <ThreeDCardDemo 
+          title={'Portfolio 2.0  - My personal portfolio website'} 
+          tech={'React Js, Typescript, Tailwind CSS, Aceternity UI '}
+          image={protfolio2}
+          link={'#'}
+        />
+
+        <ThreeDCardDemo 
+          title={'Portfolio 1.0  - My personal portfolio website'} 
+          tech={'HTML, CSS, JavaScript, Bootstrap'}
+          image={protfolio1}
+          link={'https://pureshwargonekar.github.io/My-Resume/'}
+        />
+
+        <ThreeDCardDemo 
+          title={'Portfolio 1.0  - My personal portfolio website'} 
+          tech={'HTML, CSS, JavaScript, Bootstrap'}
+          image={protfolio1}
+          link={'https://pureshwargonekar.github.io/My-Resume/'}
+        />
+
+     </div>
+    </Container>
+  );
+}
