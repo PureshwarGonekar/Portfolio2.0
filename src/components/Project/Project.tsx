40abd7d50477fbf33ee44246b764e611df9c7118
Pureshwar Gonekar <pureshwargonekar7@gmail.com>
2024-02-29 11:58:58 +0530

    solving react-animate-on-scroll issue

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,8 +1,4 @@
-import { Container } from "./styles";
-// import githubIcon from "../../assets/github.svg"
-// import DownloadApp from '../../assets/download.png'
-import externalLink from "../../assets/external-link.svg"
-import ScrollAnimation from "react-animate-on-scroll";
+import { Container } from "./styles"
 
 import { ThreeDCardDemo } from "./ProjectCard";
 
